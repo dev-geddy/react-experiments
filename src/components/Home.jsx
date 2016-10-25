ae7e8c7995fd3abb0b39c944cef854e18353e65c
Gediminas <gediminasgn@gmail.com>
2016-10-25 22:35:04 +0100

    Update home page description.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-
+import {Link} from 'react-router'
 class Home extends Component {
 
   render() {
@@ -10,7 +10,17 @@ class Home extends Component {
         </header>
         <section className="page-content">
           <h3>Shortly About</h3>
-          <p>Application is built to manipulate client notes on browser local storage.</p>
+          <p>Application is built to manage client notes and store them on browser local storage.</p>
+          <p>Expected functionality:</p>
+          <ul>
+            <li>See list of clients (companies)</li>
+            <li>Pick company</li>
+            <li>See list of notes taken</li>
+            <li>Be able to add notes for selected company</li>
+            <li>Notes can not be added if "Note creator" or "Note" is missing.</li>
+            <li>No error handling has been implemented.</li>
+          </ul>
+          <p>Take me to <Link to="notes">Notes</Link>.</p>
         </section>
       </article>
     )
